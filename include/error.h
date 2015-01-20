58c0ad373284d7d3f79754c9cfdd48704a2b7531
Eero Leno <eero@leno.fi>
2015-01-21 01:32:08 +0200

    committing to see if g++ compiles this correctly
    also adding all changes

---
+++ include/error.h
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  error.h
+ *
+ *    Description:  I shouldn't be allowed to program games.
+ *
+ *        Version:  1.0
+ *         Author:  Eero Leno
+ *         Github:  Rimminen
+ *        Created:  22.11.2014 15.52.18
+ *       Revision:  none
+ *       Compiler:  clang plus plus or gee plus plus
+ *
+ * =====================================================================================
+ */
+
+#pragma once
+
+#include "thirdincludes.h"
+
+void logError(string);
